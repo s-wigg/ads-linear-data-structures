99d1ecddf688e83447533a6fe485f2475f9ff3c3
Severin Wiggenhorn <severin@convoy.com>
2020-05-23 21:06:15 -0700

    adding implementation for doubly linked list

--- src/data_structures/doubly_linked_list.js
+++ src/data_structures/doubly_linked_list.js
@@ -1,5 +1,10 @@
 class DLLNode {
-  constructor({ element = undefined, next = this, prev = this, isSentinel = false }) {
+  constructor({
+    element = undefined,
+    next = this,
+    prev = this,
+    isSentinel = false,
+  }) {
     this.element = element;
     this.next = next;
     this.prev = prev;
@@ -31,24 +36,90 @@ class DoublyLinkedList {
   }
 
   insertHead(element) {
+    if (this._head()._active) {
+      const prevHead = this._head();
+      const newNode = new DLLNode({
+        element,
+        next: prevHead,
+        prev: this._sentinel,
+      });
+      prevHead.prev = newNode;
+      this._sentinel.next = newNode;
+      return newNode;
+    } else {
+      const newNode = new DLLNode({
+        element,
+        next: this._sentinel,
+        prev: this._sentinel,
+      });
+      this._sentinel.next = newNode;
+      this._sentinel.prev = newNode;
+      return newNode;
+    }
   }
 
   insertTail(element) {
+    if (this._tail()._active) {
+      const prev = this._tail();
+      const newNode = new DLLNode({ element, next: this._sentinel, prev });
+      prev.next = newNode;
+      this._sentinel.prev = newNode;
+      return newNode;
+    } else {
+      const newNode = new DLLNode({
+        element,
+        next: this._sentinel,
+        prev: this._sentinel,
+      });
+      this._sentinel.next = newNode;
+      this._sentinel.prev = newNode;
+      return newNode;
+    }
   }
 
   removeHead() {
+    if (this._head()._active) {
+      let oldHead = this._head();
+      this._sentinel.next = oldHead.next;
+      oldHead.next.previous = this._sentinel;
+      return oldHead.remove();
+    }
   }
 
   removeTail() {
+    if (this._tail()._active) {
+      let oldTail = this._tail();
+      this._sentinel.prev = oldTail.prev;
+      oldTail.prev.next = this._sentinel;
+      return oldTail.remove();
+    }
   }
 
   remove(node) {
+    if (node._active) {
+      node.remove();
+      return "removed";
+    }
   }
 
   forEach(callback) {
+    let node = this._head();
+    let index = 0;
+    while (node._active) {
+      callback(node.element, index, this);
+      node = node.next;
+      index += 1;
+    }
   }
 
   count() {
+    let i = 0;
+    let node = this._head();
+    while (node._active) {
+      i += 1;
+      node = node.next;
+    }
+    return i;
   }
 }
 
